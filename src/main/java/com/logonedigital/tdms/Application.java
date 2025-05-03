fec7d6374cf37b7fd85f0549f3cffb5236fe49d7
mintoua <toupkandimintoua@gmail.com>
2025-05-03 12:42:07 +0100

    config-chore(global-project): add application profiles config
    - Update compose.yaml
    - refactor Application
    - structured project in clean architecture

--- src/main/java/com/logonedigital/tdms/Application.java
+++ src/main/java/com/logonedigital/tdms/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TdmsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TdmsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
